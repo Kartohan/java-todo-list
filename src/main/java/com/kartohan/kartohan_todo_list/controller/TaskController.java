af1e2a2633aeb5c035f2b3676aed6ffc23fbf2c6
Kartohan <Kartohan@gmail.com>
2025-08-16 20:50:21 +0300

    Created Read one api call

--- src/main/java/com/kartohan/kartohan_todo_list/controller/TaskController.java
+++ src/main/java/com/kartohan/kartohan_todo_list/controller/TaskController.java
@@ -4,7 +4,12 @@ import com.kartohan.kartohan_todo_list.repository.TaskRepository;
 import com.kartohan.kartohan_todo_list.todo.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -19,5 +24,19 @@ public class TaskController {
     @ResponseStatus(HttpStatus.CREATED)
     public Task createTask(@RequestBody Task task) {
         return taskRepository.save(task);
+    }
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            return ResponseEntity.ok(task);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
