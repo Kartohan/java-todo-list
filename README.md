56304334197e1924d6848f9f23e87d8f2dc3c76a
Kartohan <Kartohan@gmail.com>
2025-08-16 21:53:43 +0300

    Crated Update and Delete API calls. Updated ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# java-todo-list
-To-Do list API
+# To Do List API
+Hello!
+Welcome to my next project where i am exploring Java!
+It's my first Java project where i am trying everything on hand and figuring out how everything works.
+
+This is pretty basic project, i set several tasks for myself for this project:
+1. Learn and explore Spring library 🍃
+2. Learn Maven and how building application in Java works
+3. Teach myself to use Git and GitHub properly. As you can see i am getting myself comfortable to create and merge PR.
+
+Things to do:
+1. Authentication for the API, JWT.
+2. Put everything into Docker.
+
+Thank you for checking out! Feel free to explore!
+
+You can contact me on Discord: Kartoshechka
+Or you can use any of the contacts in the profile!
--- src/main/java/com/kartohan/kartohan_todo_list/controller/TaskController.java
+++ src/main/java/com/kartohan/kartohan_todo_list/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.kartohan.kartohan_todo_list.controller;
 
 import com.kartohan.kartohan_todo_list.repository.TaskRepository;
 import com.kartohan.kartohan_todo_list.todo.model.Task;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +35,30 @@ public class TaskController {
         Optional<Task> optionalTask = taskRepository.findById(id);
         if (optionalTask.isPresent()) {
             Task task = optionalTask.get();
+            return ResponseEntity.ok(task);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTaskById(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task existingTask = optionalTask.get();
+            existingTask.setTitle(updatedTask.getTitle());
+            existingTask.setCompleted(updatedTask.isCompleted());
+            Task savedTask = taskRepository.save(existingTask);
+            return ResponseEntity.ok(savedTask);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Task> deleteTaskById(@PathVariable Long id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            taskRepository.deleteById(id);
             return ResponseEntity.ok(task);
         } else {
             return ResponseEntity.notFound().build();
